8cda81fff02a25f61e2ff652156b3728c887dfd7
Nicko <nickos@iMac.local>
2022-05-06 11:55:06 +0200

    test map & fetch & components

--- src/App.vue
+++ src/App.vue
@@ -1,125 +1,61 @@
 <script setup>
 import { RouterLink, RouterView } from "vue-router";
-import HelloWorld from "@/components/HelloWorld.vue";
+import countryCard from './components/countryCard.vue';
+</script>
+
+<script>
+export default {
+  components: { countryCard },
+	data() {
+		return {
+			isVisible: false,
+			items: [{ message: "oui" }, { message: "non" }, { message: "ok" }],
+			countries: [{}],
+			isLoaded: false,
+		};
+	},
+	methods: {
+		changeVisible() {
+			this.isVisible = !this.isVisible;
+		},
+
+		async getCountries() {
+			const res = await fetch("https://restcountries.com/v3.1/all");
+			const data = await res.json();
+			console.log(data);
+			this.countries = data;
+			this.isLoaded = true;
+			console.log(this.countries);
+		},
+	},
+
+	mounted() {
+		this.getCountries();
+	},
+};
 </script>
 
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="@/assets/logo.svg"
-      width="125"
-      height="125"
-    />
+	<h1>Welcome tot he space X Api</h1>
+	<button @click="changeVisible">Click me</button>
+	<h2 v-if="this.isVisible">hello</h2>
+	<h2 v-else>GoodBye</h2>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+	<ul v-if="isLoaded">
+		<li v-for="(country, i) in countries" :key="i">
+      <country-card :countryName="country.name.common" :flag="country.flag" />
+		</li>
+	</ul>
+  <p v-else>loading</p>
 </template>
 
 <style>
 @import "@/assets/base.css";
 
 #app {
-  max-width: 1280px;
-  margin: 0 auto;
-  padding: 2rem;
-
-  font-weight: normal;
 }
 
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-a,
-.green {
-  text-decoration: none;
-  color: hsla(160, 100%, 37%, 1);
-  transition: 0.4s;
-}
-
-@media (hover: hover) {
-  a:hover {
-    background-color: hsla(160, 100%, 37%, 0.2);
-  }
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  body {
-    display: flex;
-    place-items: center;
-  }
-
-  #app {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    padding: 0 2rem;
-  }
-
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+li {
+	margin: 0;
 }
 </style>
---
+++ src/components/countryCard.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+	props: {
+		countryName: String,
+        flag: String
+	},
+};
+</script>
+
+<template>
+	<p>{{ countryName }}</p>
+	<p>{{ flag }}</p>
+</template>
